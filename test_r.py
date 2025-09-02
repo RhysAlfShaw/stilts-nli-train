5922e40080412e4ebf6b7f3918d53a052c23f971
Rhys Shaw <rhysc3p0@gmail.com>
2025-09-02 08:58:29 +0100

    cleaning up

--- test_r.py
+++
@@ -1,8 +0,0 @@
-from llama_cpp import Llama
-
-# Download and load a GGUF model directly from Hugging Face
-llm = Llama.from_pretrained()
-response = llm.create_chat_completion(
-    messages=[{"role": "user", "content": "How does a black hole work?"}]
-)
-print(response)
